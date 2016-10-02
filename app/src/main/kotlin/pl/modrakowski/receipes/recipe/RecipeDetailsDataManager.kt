70c8c909f29d5f8f04e41fcfdf4988081460479a
Jacek Modrakowski <jacek.beny@gmail.com>
2016-10-02 20:59:04 +0200

    Fixes DBFlow usage.

--- app/src/main/kotlin/pl/modrakowski/receipes/recipe/RecipeDetailsDataManager.kt
+++ app/src/main/kotlin/pl/modrakowski/receipes/recipe/RecipeDetailsDataManager.kt
@@ -1,12 +1,18 @@
 package pl.modrakowski.receipes.recipe
 
+import com.raizlabs.android.dbflow.kotlinextensions.from
+import com.raizlabs.android.dbflow.kotlinextensions.select
+import com.raizlabs.android.dbflow.kotlinextensions.where
 import pl.modrakowski.receipes.common.BaseDataManager
 import pl.modrakowski.receipes.common.DataListener
 import pl.modrakowski.receipes.common.data.Recipe
+import pl.modrakowski.receipes.common.data.Recipe_Table.id
 
 class RecipeDetailsDataManager : BaseDataManager<Recipe>() {
     override fun loadData(params: Any?, listener: DataListener<Recipe>) {
-        listener.onDataLoadedSuccessfully(Recipe(1L, "Schabowy",
-                "http://karczmazabidwor.pl/files/page/zdjecia/potrawy/kotlet-%20schabowy.jpg"))
+        val idVal: Long = params as Long
+        listener.onDataLoadedSuccessfully((
+                select from Recipe::class where id.eq(idVal)
+                ).querySingle()!!)
     }
 }
